43c399fecc34c8ea23f26c1c60f89df2df2cd3c9
EtienneV <etienne.vaytilingom@gmail.com>
2021-08-09 14:53:59 +0400

    V1.2 - affichage alerte et passage premiere activite ok

--- app/src/main/java/re/etienne/projet1/MainActivity2.kt
+++ app/src/main/java/re/etienne/projet1/MainActivity2.kt
@@ -1,16 +1,19 @@
 package re.etienne.projet1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Initialisation des vues
         val btR1 = findViewById<Button>(R.id.btR1)
         val btR2 = findViewById<Button>(R.id.btR2)
         val btR3 = findViewById<Button>(R.id.btR3)
@@ -18,21 +21,44 @@ class MainActivity2 : AppCompatActivity() {
         val tvQuestion = findViewById<TextView>(R.id.tvQuestion)
         val tvCpt = findViewById<TextView>(R.id.tvCpt)
 
-        tvCpt.text = "1" //1.toString()
+        //Modification texte
+        tvCpt.text = "2" //2.toString()
         tvQuestion.text = "Quelle est la meilleur équipe de foot française ?"
         btR1.text = "Jeanne D'arc"
         btR2.text = "OM"
         btR3.text = "Saint Louisienne"
         btR4.text = "La tamponnaise"
 
-        val toastMauvaiseReponse = Toast.makeText(this,
-                "Mauvaise reponse", Toast.LENGTH_SHORT)
-        val toastBonneReponse = Toast.makeText(this,
-                "Bonne reponse", Toast.LENGTH_SHORT)
 
-        btR1.setOnClickListener { toastMauvaiseReponse.show() }
-        btR2.setOnClickListener { toastBonneReponse.show() }
-        btR3.setOnClickListener { toastMauvaiseReponse.show() }
-        btR4.setOnClickListener { toastMauvaiseReponse.show() }
+        //Intent
+        val intent = Intent(this,MainActivity::class.java)
+        
+        //Alerte
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Merci ! ")
+        builder.setCancelable(false) // Oblige le clique sur un bouton 
+        builder.setPositiveButton("Oui") { dialog, which ->
+            startActivity(intent)
+        }
+
+        //Function affichant un toast et demarrant la prochaine activite
+        fun alertintent(reponse: Boolean){
+            
+            //builder.show() builder.setMessage("Rejouer ?")
+            if(reponse){
+                builder.setMessage("Super! Bonne réponse ! Rejouer ?")
+                builder.show()
+            }
+            else{
+                builder.setMessage("Mince! Mauvaise réponse ! Rééssayer ?")
+                builder.show()
+            }
+        }
+
+        //Button listener
+        btR1.setOnClickListener { alertintent(false)}
+        btR2.setOnClickListener { alertintent(true)}
+        btR3.setOnClickListener { alertintent(false)}
+        btR4.setOnClickListener { alertintent(false)}
     }
 }
